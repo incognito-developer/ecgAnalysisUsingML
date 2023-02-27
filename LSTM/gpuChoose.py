3276c5eda6621017e406f765708fa4efeea74e70
incognito-developer <43435087+incognito-developer@users.noreply.github.com>
2023-02-27 15:23:26 +0900

    Create gpuChoose.py
    gpu choose example for tensorflow

---
+++ LSTM/gpuChoose.py
@@ -0,0 +1,8 @@
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+    tf.config.experimental.set_memory_growth(gpus[0], True)
+  except RuntimeError as e:
+    # 프로그램 시작시에 메모리 증가가 설정되어야만 합니다
+    print(e)
